8140c083bdfe7748df754e12cc4f45229e561c40
alexkay7133 <rsxr0509@leeds.ac.uk>
2026-02-17 16:52:23 +0000

    Worksheet attempt 1

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -11,26 +11,26 @@ int main( void ) {
 
     // define and initialise variables for the problem data 
     float salary = 36250.00;
-    int NI_percent = 8;
-    int tax_percent = 15;
+    float NI_percent = 8;
+    float tax_percent = 15;
 
-    float final_salary = 0;
-    int NI_amount = 0;
-    int tax_amount = 0;
+    float final_salary;
+    float NI_amount;
+    float tax_amount;
 
     // calculate the deductions and final take-home salary
     NI_amount = salary * (NI_percent / 100);
-    final_salary = salary - NI_amount
+    final_salary = salary - NI_amount;
     if (final_salary > 12500) {
-        tax_amount = (final_salary - 12500) * (tax_percent / 100)
-        final_salary = final_salary - tax_amount
+       tax_amount = (final_salary - 12500) * (tax_percent / 100);
+       final_salary = final_salary - tax_amount;
     }
 
     // Use only these print statement with appropriate formatting and variable names
-    printf("Salary £%.2f",salary);
-    printf("NI contribution £%.2f",NI_amount);
-    printf("Tax contribution £%.2f",tax_amount);
-    printf("Take home salary £%.2f",final_salary);
+    printf("Salary £%.2f \n",salary);
+    printf("NI contribution £%.2f \n",NI_amount);
+    printf("Tax contribution £%.2f \n",tax_amount);
+    printf("Take home salary £%.2f \n",final_salary);
 
     return 0;
- }
+}
